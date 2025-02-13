efacec4f39eac88fede5f0e61f836e6262e003ed
jasuni69 <jasonnicolini@outlook.com>
2025-02-13 20:57:49 +0100

    changed main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,24 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from database import get_db
 from models import CPU
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/api/cpus")
+class CPUModel(BaseModel):
+    id: int
+    name: str
+    brand: str
+    socket: str
+    cores: int
+    threads: int
+    base_clock: float
+    cache: float
+    price: float
+
+    class Config:
+        from_attributes = True  # Fixes serialization issue
+
+@app.get("/cpus", response_model=list[CPUModel])
 def get_cpus(db: Session = Depends(get_db)):
     return db.query(CPU).all()
