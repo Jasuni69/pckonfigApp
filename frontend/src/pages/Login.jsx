e09fa4773423f78a682f8b64c6cba18226a145ba
jasuni69 <jasonnicolini@outlook.com>
2025-03-27 22:31:05 +0100

    forgot to import config.js storing the ipadress...

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { API_URL } from '../config';
 
 const Login = () => {
   const [formData, setFormData] = useState({
